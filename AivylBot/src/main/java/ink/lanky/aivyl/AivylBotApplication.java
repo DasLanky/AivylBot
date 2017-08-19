808bc30799c37cd0f4344fb72de7dc93f75de08a
DasLanky <gcubewhiz@hotmail.com>
2017-08-18 19:48:35 -0700

    Initial commit

---
+++ AivylBot/src/main/java/ink/lanky/aivyl/AivylBotApplication.java
@@ -0,0 +1,12 @@
+package ink.lanky.aivyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AivylBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AivylBotApplication.class, args);
+	}
+}
